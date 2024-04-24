d9c2d146bc81ab83e6e073a35a9690fe7101b664
maji <150906506+maji-git@users.noreply.github.com>
2024-04-24 13:17:11 +0700

    Rewrite test

--- tests/PasswordAuth.gd
+++ tests/PasswordAuth.gd
@@ -1,19 +1,10 @@
 extends Node
 
-var auth: MPAuth
+@export var auth: MPAuth
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	auth = get_parent()
-	
 	auth.authenticate_function = _auth_check
 
-func _auth_check(plr_id, credentials_data, handshake_data):
-	print(credentials_data)
-	if !credentials_data.keys().has("password"):
-		return false
-	return {username = credentials_data.password }
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _auth_check(plr_id: int, credentials_data: Dictionary, handshake_data: Dictionary):
+	# Return authentication data, otherwise if failed, return false.
+	return {}
