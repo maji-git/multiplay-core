cb1dbd70eaa723a61a8e221dfddbbd3e0c884a09
maji <150906506+maji-git@users.noreply.github.com>
2024-05-05 13:15:10 +0700

    private internal variables in latency protocol

--- addons/MultiplayCore/net_protocols/LatencyNetProtocol.gd
+++ addons/MultiplayCore/net_protocols/LatencyNetProtocol.gd
@@ -12,27 +12,27 @@ class_name LatencyNetProtocol
 ## Simulate Chance of Network Loss
 @export_range(0, 0.5) var simulate_loss : float = 0
 
-var virtual_port = -1
-var true_server_port = -1
+var _virtual_port = -1
+var _true_server_port = -1
 
-var vserver_peer : PacketPeerUDP
-var vserver_has_dest_address : bool = false
-var vserver_first_client_port : int = -1
-var vclient_peer : PacketPeerUDP
+var _vserver_peer : PacketPeerUDP
+var _vserver_has_dest_address : bool = false
+var _vserver_first_client_port : int = -1
+var _vclient_peer : PacketPeerUDP
 
 func _ready():
 	super()
 	if Engine.is_editor_hint():
 		return
 	
-	true_server_port = mpc.port
-	virtual_port = mpc.port + 1
+	_true_server_port = mpc.port
+	_virtual_port = mpc.port + 1
 
-	vserver_peer = PacketPeerUDP.new()
-	vserver_peer.bind(virtual_port, "127.0.0.1")
+	_vserver_peer = PacketPeerUDP.new()
+	_vserver_peer.bind(_virtual_port, "127.0.0.1")
 	
-	vclient_peer = PacketPeerUDP.new()
-	vclient_peer.set_dest_address("127.0.0.1", mpc.port)
+	_vclient_peer = PacketPeerUDP.new()
+	_vclient_peer.set_dest_address("127.0.0.1", mpc.port)
 
 	if not OS.is_debug_build():
 		MPIO.logwarn("LatencyNetProtocol is currently in use! Please change network protocol in the production build!")
@@ -49,7 +49,7 @@ func host(port, bind_ip, max_players) -> MultiplayerPeer:
 
 func join(address, port) -> MultiplayerPeer:
 	var peer = ENetMultiplayerPeer.new()
-	var err = peer.create_client("127.0.0.1", virtual_port)
+	var err = peer.create_client("127.0.0.1", _virtual_port)
 	
 	return peer
 
@@ -61,8 +61,8 @@ class QueueEntry :
 		self.byte_array = packet
 		self.qued_at = time_now
 
-var client_to_server_queue : Array[QueueEntry]
-var server_to_client_queue : Array[QueueEntry]
+var _client_to_server_queue : Array[QueueEntry]
+var _server_to_client_queue : Array[QueueEntry]
 
 func _process(_delta : float) -> void :
 	if Engine.is_editor_hint():
@@ -71,48 +71,48 @@ func _process(_delta : float) -> void :
 	var send_at_ms = now - simulate_latency_ms
 	
 	# Handle packets Client -> Server
-	while vserver_peer.get_available_packet_count() > 0 :
-		var packet = vserver_peer.get_packet()
-		var err = vserver_peer.get_packet_error()
+	while _vserver_peer.get_available_packet_count() > 0 :
+		var packet = _vserver_peer.get_packet()
+		var err = _vserver_peer.get_packet_error()
 		if err != OK :
 			push_error("DebugUDPLagger : Incoming packet error : ", err)
 			continue
 			
-		var from_port = vserver_peer.get_packet_port()
+		var from_port = _vserver_peer.get_packet_port()
 		
-		if not vserver_has_dest_address : 
+		if not _vserver_has_dest_address : 
 			# Assume the first who send a packet to us is the True Client
-			vserver_peer.set_dest_address("127.0.0.1", from_port)
-			vserver_first_client_port = from_port
-			vserver_has_dest_address = true
-		elif vserver_first_client_port != from_port :
+			_vserver_peer.set_dest_address("127.0.0.1", from_port)
+			_vserver_first_client_port = from_port
+			_vserver_has_dest_address = true
+		elif _vserver_first_client_port != from_port :
 			push_warning("DebugUDPLagger : VServer got packet from unknown port, ignored.")
 			continue
 		
-		client_to_server_queue.push_back(QueueEntry.new(packet, now))
+		_client_to_server_queue.push_back(QueueEntry.new(packet, now))
 	
-	_process_queue(client_to_server_queue, vclient_peer, send_at_ms)
+	_process_queue(_client_to_server_queue, _vclient_peer, send_at_ms)
 	
 	# Ignore check for any incoming packets from the true server
-	if not vserver_has_dest_address :
+	if not _vserver_has_dest_address :
 		return
 	
 	# Handle packets Server -> Client
-	while vclient_peer.get_available_packet_count() > 0 :
-		var packet = vclient_peer.get_packet()
-		var err = vclient_peer.get_packet_error()
+	while _vclient_peer.get_available_packet_count() > 0 :
+		var packet = _vclient_peer.get_packet()
+		var err = _vclient_peer.get_packet_error()
 		if err != OK :
 			push_error("DebugUDPLagger : Incoming packet error : ", err)
 			continue
 		
-		var from_port = vclient_peer.get_packet_port()
-		if from_port != true_server_port :
+		var from_port = _vclient_peer.get_packet_port()
+		if from_port != _true_server_port :
 			push_warning("DebugUDPLagger : VClient got packet from unknown port, ignored.")
 			continue
 		
-		server_to_client_queue.push_back(QueueEntry.new(packet, now))
+		_server_to_client_queue.push_back(QueueEntry.new(packet, now))
 
-	_process_queue(server_to_client_queue, vserver_peer, send_at_ms)
+	_process_queue(_server_to_client_queue, _vserver_peer, send_at_ms)
 	
 	
 func _process_queue(que : Array[QueueEntry], to_peer : PacketPeerUDP, send_at_ms : int) :
