7564c76f8693654931a06ed1ef17efa17272392b
maji <150906506+maji-git@users.noreply.github.com>
2024-05-17 23:34:39 +0700

    Add Compression for ENetProtocol

--- addons/MultiplayCore/net_protocols/ENetProtocol.gd
+++ addons/MultiplayCore/net_protocols/ENetProtocol.gd
@@ -4,16 +4,26 @@ extends MPNetProtocolBase
 ## Websocket Network Protocol
 class_name ENetProtocol
 
+## Set ENet host compression mode
+@export var compression_mode: ENetConnection.CompressionMode = ENetConnection.COMPRESS_ZLIB
+
+func _ready():
+	super()
+
 ## Host function
 func host(port, bind_ip, max_players) -> MultiplayerPeer:
 	var peer = ENetMultiplayerPeer.new()
 	peer.set_bind_ip(bind_ip)
 	peer.create_server(port, max_players)
 	
+	peer.host.compress(compression_mode)
+	
 	return peer
 
 func join(address, port) -> MultiplayerPeer:
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_client(address, port)
+	
+	peer.host.compress(compression_mode)
 	
 	return peer
